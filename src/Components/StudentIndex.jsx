cddb6656b1c57b8acac96e333ef1585e06bb851b
jiminey <jtwnguyen@ucdavis.edu>
2019-11-14 11:04:50 -0800

    Create fetch

---
+++ src/Components/StudentIndex.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import StudentIndexItem from './StudentIndexItem'
+import './StudentIndex.css'
+
+export default class Student extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            students: [],
+            name_keyword: '', 
+            tag_keyword: '',
+            matches: [],
+
+        }
+        this.handleName = this.handleName.bind(this)
+        this.handleTag = this.handleTag.bind(this)
+    }
+
+    componentDidMount() {
+        axios.get('https://www.hatchways.io/api/assessment/students').then(res => {
+            this.setState({ students: res.data.students })
+        })
+    }
+
+    handleName() {
+
+    }
+    
+    handleTag() {
+
+    }
+
+    render() {
+        const students = this.state.students.map(student => {
+            return (
+                <StudentIndexItem
+                  key={student.id}
+                  id={student.id}
+                  pic={student.pic}
+                  firstName={student.firstName}
+                  lastName={student.lastName}
+                  email={student.email}
+                  company={student.company}
+                  skill={student.skill}
+                  grades={student.grades}
+                  tags={student.tags}
+                  // handleTags={this.handleTags}
+                ></StudentIndexItem>
+            );
+        })
+
+        return (
+          <div>
+              <div>
+                <form>
+                    <input type="text" 
+                    onChange={this.handleName} 
+                    placeholder='Search by name...' 
+                    value={this.state.name_keyword}/>
+                </form>
+              </div>
+              <div>
+                <form>
+                    <input type="text" 
+                    onChange={this.handleTag} 
+                    placeholder='Search by tags...' 
+                    value={this.state.tag_keyword}/>
+                </form>
+              </div>
+              
+          </div>
+        );
+    }
+}
---
+++ src/Components/StudentIndexItem.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './StudentIndexItem.css'
+
+export default class StudentIndexItem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
