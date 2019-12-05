647bea154230ca66ba60623c33cfb4f84fbeeae3
jiminey <jtwnguyen@ucdavis.edu>
2019-12-05 04:16:07 -0800

    Updates

---
+++ precache-manifest.e034599478e0548bd90a1cf8e6391a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdcbc0ed26d9444cafc57d3901bdbb60",
+    "url": "/DataFetch/index.html"
+  },
+  {
+    "revision": "ce75ef710f079fc1da70",
+    "url": "/DataFetch/static/css/main.36d96720.chunk.css"
+  },
+  {
+    "revision": "c1b44ae7867a2ba75ae0",
+    "url": "/DataFetch/static/js/2.a3c1d535.chunk.js"
+  },
+  {
+    "revision": "ce75ef710f079fc1da70",
+    "url": "/DataFetch/static/js/main.6ad0e201.chunk.js"
+  },
+  {
+    "revision": "1f8bb2203200d0770c60",
+    "url": "/DataFetch/static/js/runtime-main.e9b9edee.js"
+  }
+]);
